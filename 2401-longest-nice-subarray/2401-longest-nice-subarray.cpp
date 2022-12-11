8fc58f9218e13b4c1f4543f39604fcfdcde9cd9c
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2022-12-12 00:02:46 +0530

    Time: 288 ms (66.06%), Space: 57.2 MB (62.67%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        int used = 0, i = 0, j = 0, res = 0;
-        for(; i < nums.size(); i++) {
+        int used = 0, j = 0, res = 0;
+        for(int i=0; i < nums.size(); i++) {
             while(used & nums[i]) {
                 used ^= nums[j];
                 j++;
