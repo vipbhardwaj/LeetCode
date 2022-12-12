ca3977a122647d4a7bc15839ecd0d561280a0c22
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2022-12-12 12:37:36 +0530

    Time: 4 ms (21.07%), Space: 6.1 MB (38.51%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    
+    int climbStairs(int n) {
+        int steps[n];
+        steps[0] = 1;
+        if(n > 1)
+            steps[1] = 2;
+        
+        for(int i=2; i<n; i++)
+            steps[i] = steps[i-1] + steps[i-2];
+        
+        return steps[n-1];
+    }
+};
