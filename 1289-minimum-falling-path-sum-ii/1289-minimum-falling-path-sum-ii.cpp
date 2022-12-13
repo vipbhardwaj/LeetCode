75fbc1775ac83e26e14a247f8cd8dc71e6a59638
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2022-12-13 15:26:43 +0530

    Time: 694 ms (60.78%), Space: 15.9 MB (44.00%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
-        // vector<vector<vector<int>>> dp(grid.size(), vector<vector<int>>{grid});
         vector<vector<int>> dp = grid;
-        for(int j=1; j < dp.size(); j++) {
+        for(int j=1; j < dp.size(); j++)
             for(int k=0; k < dp.size(); k++) {
                 int e = INT_MAX;
                 for(int x=0; x < dp.size(); x++) {
@@ -12,21 +11,7 @@ public:
                     e = min(e, dp[j-1][x]);
                 }
                 dp[j][k] += e;
-            }
         }
         return *min_element(begin(dp[dp.size() - 1]), end(dp[dp.size() - 1]));
-        // for(int i=0; i < dp.size(); i++) {
-        //     for(int j=1; j < dp.size(); j++) {
-        //         for(int k=0; k < dp.size(); k++) {
-        //             int e = INT_MAX;
-        //             for(int x=0; x < dp.size(); x++) {
-        //                 if(x == k)
-        //                     continue;
-        //                 e = min(e, dp[i][j-1][x]);
-        //             }
-        //             dp[i][j][k] += e;
-        //         }
-        //     }
-        // }
     }
 };
