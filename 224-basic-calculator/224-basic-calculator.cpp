c1d79bcc5161b282e233bd74a050cd026575a045
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2022-12-19 00:23:34 +0530

    Time: 23 ms (40.13%), Space: 7.9 MB (91.42%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> op{{1}};
+        int num = 0, total = 0, isPositive = 1;
+        
+        for(char c: s) {
+            if(isdigit(c))
+                num = num * 10 + (c - '0');
+            else {
+                total += num * isPositive * op.top();
+                num = 0;
+                if(c == '+')
+                    isPositive = 1;
+                else if(c == '-')
+                    isPositive = -1;
+                else if(c == ')')
+                    op.pop();
+                else if(c == '(') {
+                    op.push(op.top() * isPositive);
+                    isPositive = 1;
+                }
+            }
+        }
+        total += num * isPositive * op.top();
+        return total;
+    }
+};
