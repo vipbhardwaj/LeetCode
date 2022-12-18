d473e194b618786e9beb8ef1cbd774ca7882bc6e
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2022-12-18 19:38:53 +0530

    Time: 617 ms (5.02%), Space: 98.7 MB (58.21%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -5,16 +5,12 @@ public:
         map<int, int> index;
         index[t[t.size() - 1]] = t.size() - 1;
         vector<int> v(t.size(), 0);
-        int minIndex, maxTemp = t[t.size() - 1];
+        int minIndex;
         
         for(int i = t.size() - 2; i >= 0; i--) {
             index[t[i]] = i;
-            maxTemp = max(maxTemp, t[i]);
             if(t[i] < t[i+1]) {
                 v[i] = 1;
-                continue;
-            } if(t[i] == maxTemp) {
-                v[i] = 0;
                 continue;
             }
             auto it = index.find(t[i]);
