e6a62231455c9a1d5d491ac35c5c77d24f3346bf
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2022-12-18 19:23:44 +0530

    Time: 853 ms (5.02%), Space: 98.7 MB (74.20%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& t) {
-        set<int> high = {t[t.size() - 1]};
-        unordered_map<int, int> index;
+        // set<int> high = {t[t.size() - 1]};
+        map<int, int> index;
         index[t[t.size() - 1]] = t.size() - 1;
         vector<int> v(t.size(), 0);
         int minIndex;
         
         for(int i = t.size() - 2; i >= 0; i--) {
-            high.insert(t[i]);
             index[t[i]] = i;
-            auto it = high.find(t[i]);
+            auto it = index.find(t[i]);
             minIndex = INT_MAX;
             
-            while(++it != high.end())
-                minIndex = min(minIndex, index[*it]);
+            while(++it != index.end())
+                minIndex = min(minIndex, it->second);
             if(minIndex != INT_MAX)
                 v[i] = minIndex - i;
         }
