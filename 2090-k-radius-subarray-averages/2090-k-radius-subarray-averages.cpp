ef7cb80dc0380d180fedc2de291a0f641d65f167
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2023-06-20 11:17:29 +0530

    Time: 231 ms (93.82%), Space: 136 MB (48.49%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n < 2*k + 1)
+            return vector<int>(n, -1);
+        
+        long long winSum = 0;
+        vector<int> v(k, -1);
+        
+        for(int i=0; i < (2*k + 1); i++)
+            winSum += nums[i];
+        for(int i=k; i < n-k; i++) {
+            v.push_back(winSum / (2*k + 1));
+            winSum -= nums[i - k];
+            if(n > i+k+1)
+                winSum += nums[i + k + 1];
+            else
+                break;
+        }
+        
+        while(k--)
+            v.push_back(-1);
+        return v;
+    }
+};
