e1f940242985ec3749d0e43bf0ce7f44a07b56be
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2024-05-01 13:52:37 +0530

    Time: 1 ms (76.52%), Space: 2.3 MB (74.78%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.go
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.go
@@ -1,9 +1,7 @@
 func reversePrefix(word string, ch byte) string {
     chi := -1
-    // substr := ""
 
     for i, c := range word {
-        // substr += string(c)
         if byte(c) == ch {
             chi = i
             break
@@ -20,20 +18,6 @@ func reversePrefix(word string, ch byte) string {
         bytes[i] = bytes[chi-i]
         bytes[chi-i] = temp
     }
-    // fmt.Println(string(bytes))
-    // fmt.Println(word[chi:])
+
     return string(bytes)
 }
-
-// func reverse(str string) string {
-//     n := len(str)
-//     for i := range n {
-//         if i == n/2 {
-//             break
-//         }
-//         temp := str[i]
-//         str[i] = str[n-i]
-//         str[n-i] = temp
-//     }
-//     return str
-// }
