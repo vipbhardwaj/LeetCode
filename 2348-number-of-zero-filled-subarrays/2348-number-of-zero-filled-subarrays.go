685eb045ce45e0dad15f5c115fa009dd60b691f6
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2024-05-05 12:40:06 +0530

    Time: 170 ms (5.45%), Space: 8.6 MB (45.45%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.go
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.go
@@ -1,13 +1,13 @@
 func zeroFilledSubarray(nums []int) int64 {
-    sum := 0
+    var sum int64
     var res int64
     for _, n := range nums {
         if n == 0 {
             sum += 1
         } else {
-            res += int64(sum * (sum+1) / 2)
+            res += sum * (sum+1) / 2
             sum = 0
         }
     }
-    return res + int64(sum * (sum+1) / 2)
+    return res + (sum * (sum+1) / 2)
 }
