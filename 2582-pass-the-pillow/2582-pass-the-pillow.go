0f586c6361fd72bd0e1383be49df4e89e858403f
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2024-07-06 13:09:37 +0530

    Time: 0 ms (100.00%), Space: 2.1 MB (46.15%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.go
+++ 2582-pass-the-pillow/2582-pass-the-pillow.go
@@ -1,4 +1,20 @@
 func passThePillow(n int, time int) int {
+    return O1(n, time)
+    return On(n, time)
+}
+
+func O1(n int, time int) int {
+    side := time / (n-1)
+    time %= (n-1)
+    
+    if side % 2 == 0 {
+        return time+1
+    } else {
+        return n-time
+    }
+}
+
+func On(n int, time int) int {
     side := true
     for time >= n {
         time -= (n-1)
@@ -10,5 +26,4 @@ func passThePillow(n int, time int) int {
     } else {
         return n-time
     }
-    
 }
