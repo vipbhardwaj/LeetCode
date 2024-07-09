8e6dfe080ef02bb290450865e8f3d89f83409282
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2024-07-09 22:36:34 +0530

    Time: 115 ms (58.33%), Space: 13.9 MB (100.00%) - LeetHub

--- 1701-average-waiting-time/1701-average-waiting-time.go
+++ 1701-average-waiting-time/1701-average-waiting-time.go
@@ -2,7 +2,6 @@ func averageWaitingTime(customers [][]int) float64 {
     prevTime := 0
     totalWait := 0
     for _, p := range customers {
-        // wait := 0
         if prevTime < p[0] {
             totalWait += p[1]
             prevTime = p[1] + p[0]
@@ -10,12 +9,6 @@ func averageWaitingTime(customers [][]int) float64 {
             prevTime += p[1]
             totalWait += prevTime - p[0]
         }
-
-        // fmt.Println(currTime)
-        // fmt.Println(wait)
-        // totalWait += wait
-        // fmt.Println(totalWait)
-        // fmt.Println()
     }
     return float64(totalWait) / float64(len(customers))
 }
