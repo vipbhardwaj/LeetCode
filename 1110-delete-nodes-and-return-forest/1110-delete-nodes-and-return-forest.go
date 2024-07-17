e50c3290a905f9b922f0ef586cc913f6d26136bb
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2024-07-17 22:37:18 +0530

    Time: 9 ms (52.73%), Space: 6.7 MB (43.64%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.go
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func delNodes(root *TreeNode, to_delete []int) []*TreeNode {
+    m := make(map[int]int)
+    for _, i := range to_delete {
+        m[i] = i
+    }
+    r := dfs(root, m, []*TreeNode{root})
+    var res []*TreeNode
+    for _, i := range r {
+        if _, ok := m[i.Val]; ok {
+            continue
+        }
+        res = append(res, i)
+    }
+    return res
+}
+
+func dfs(n *TreeNode, d map[int]int, r []*TreeNode) []*TreeNode {
+    if n == nil || (n.Left==nil && n.Right==nil) {
+        return r
+    }
+    
+    if _, ok := d[n.Val]; ok {
+        if n.Left != nil {
+            r = append(r, n.Left)
+        }
+        if n.Right != nil {
+            r = append(r, n.Right)
+        }
+    }
+    
+    r = dfs(n.Left, d, r)
+    r = dfs(n.Right, d, r)
+    
+    if n.Left != nil {
+        if _, ok := d[n.Left.Val]; ok {
+            n.Left = nil
+        }
+    } 
+    if n.Right != nil {
+        if _, ok := d[n.Right.Val]; ok {
+            n.Right = nil
+        }
+    }
+    if _, ok := d[n.Val]; ok {
+        if n.Left != nil {
+            n.Left = nil
+        }
+        if n.Right != nil {
+            n.Right = nil
+        }
+    }
+    
+    return r
+} 
