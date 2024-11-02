51d013dcd60ee942a1caab3f039adb6804dd9cae
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2024-11-02 23:03:39 +0530

    Time: 0 ms (100%), Space: 4.2 MB (15.15%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.go
+++ 2490-circular-sentence/2490-circular-sentence.go
@@ -1,15 +1,14 @@
 func isCircularSentence(s string) bool {
-    var last byte
+    // var last byte
     for i:=0; i < len(s); i++ {
         if s[i] != ' ' {
-            last = s[i]
             continue
         }
-        if i < len(s)-1 && s[i-1] != s[i+1] {
+        if s[i-1] != s[i+1] {
             return false
         }
     }
-    if s[0] != last {
+    if s[0] != s[len(s)-1] {
         return false
     }
     return true
