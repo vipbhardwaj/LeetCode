a9c79aa21e861ffe295e3d751131aa589b8b68c6
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2025-07-04 16:52:44 +0530

    Time: 0 ms (100%), Space: 4.6 MB (100%) - LeetHub

---
+++ 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.go
@@ -0,0 +1,20 @@
+func kthCharacter(k int64, ops []int) byte {
+    var s = int64(1)
+    var op int
+    for s < k {
+        s *= 2
+        op++
+    }
+    
+    var shifts int
+    for s > 1 {
+        op--
+        s /= 2
+        if k > s {
+            k -= s
+            shifts += ops[op]
+        }
+    }
+    // return byte(('a' + shifts) % 26) + 'a' - 19
+    return byte((('a' - 'a' + shifts) % 26) + 'a')
+}
