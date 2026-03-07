73d3cbc02fba03c83252655531afa4916a8bd7e0
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2026-03-07 22:15:47 +0530

    Time: 6 ms (64.29%), Space: 8.5 MB (50%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.go
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.go
@@ -1,39 +1,38 @@
 func minFlips(s string) int {
-    res := I(s)
-    if s[0] == s[len(s)-1] {
-        return res
-    }
-    var i int = 1
-    var b strings.Builder
-    b.Grow(len(s))
-    for; i < len(s) - 1; i++ {
-        if s[i] == s[i-1] {
-            break
-        }
-    }
-    for ; i < len(s); i++ {
-        b.WriteByte(s[i % len(s)])
-    }
-    return min(res, I(b.String()))
-}
+    n := len(s)
+    s = s + s
 
-func I(s string) int {
-    var res int
-    prev := s[0]
-    run := 1
-    for i:=1; i < len(s); i++ {
-        curr := s[i]
-        if curr != prev {
-            res += run / 2
-            run = 1
+    alt1 := make([]byte, len(s))
+    alt2 := make([]byte, len(s))
+
+    for i := range s {
+        if i%2 == 0 {
+            alt1[i] = '0'
+            alt2[i] = '1'
         } else {
-            run++
-        }
-        prev = curr
-        if i >= len(s) / 2 {
-            
+            alt1[i] = '1'
+            alt2[i] = '0'
         }
     }
-    fmt.Println(res)
-    return res + run / 2
+
+    res := n
+    diff1, diff2 := 0, 0
+    l := 0
+
+    for r := 0; r < len(s); r++ {
+        if s[r] != alt1[r] { diff1++ }
+        if s[r] != alt2[r] { diff2++ }
+
+        if r-l+1 > n {
+            if s[l] != alt1[l] { diff1-- }
+            if s[l] != alt2[l] { diff2-- }
+            l++
+        }
+
+        if r-l+1 == n {
+            res = min(res, diff1, diff2)
+        }
+    }
+
+    return res
 }
