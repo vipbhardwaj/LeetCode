200f4b26304183c45d8c5c0b3c6f4d1e75b5e14e
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2026-04-04 23:05:09 +0530

    Time: 11 ms (75%), Space: 10.3 MB (91.67%) - LeetHub

---
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.go
@@ -0,0 +1,22 @@
+func decodeCiphertext(s string, rows int) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	cols := int(math.Ceil(float64(len(s)) / float64(rows)))
+	var res []byte
+
+	for start := 0; start < cols; start++ {
+		r, c := 0, start
+		for r < rows && c < cols {
+			idx := r*cols + c
+			if idx < len(s) {
+				res = append(res, s[idx])
+			}
+			r++
+			c++
+		}
+	}
+
+	return strings.TrimRight(string(res), " ")
+}
