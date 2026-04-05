b6aa7698d1ff804e205e77de0771d17837ce3b05
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2026-04-05 16:09:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.go
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.go
@@ -2,16 +2,16 @@ func judgeCircle(moves string) bool {
     var x, y int
     for _, move := range moves {
         switch move {
-            case 'U':
-                x++
-            case 'D':
-                x--
-            case 'L':
-                y++
-            case 'R':
-                y--
-            default:
-                continue
+        case 'U':
+            x++
+        case 'D':
+            x--
+        case 'L':
+            y++
+        case 'R':
+            y--
+        default:
+            continue
         }
     }
     return x == 0 && y == 0
