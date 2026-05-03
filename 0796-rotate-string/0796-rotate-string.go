d64baf81e956b17a057cbc1cadd4cd048eb274c4
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2026-05-03 18:03:53 +0530

    Time: 0 ms (100%), Space: 4.4 MB (6.2%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.go
+++ 0796-rotate-string/0796-rotate-string.go
@@ -1,11 +1,25 @@
 func rotateString(s string, goal string) bool {
-    if len(s) != len(goal) {
-        return false
-    }
-
-    s = s + s
-    if strings.Contains(s, goal) {
+    if s == goal {
         return true
     }
+    var l int = len(s)
+    var ss strings.Builder
+    ss.WriteString(s)
+    ss.WriteString(s)
+    s = ss.String()
+
+    for i := 1; i < l; i++ {
+        if makeStr(i, s, l) == goal {
+            return true
+        }
+    }
     return false
+}
+
+func makeStr(i int, s string, n int) string {
+    var ss strings.Builder
+    for j := 0; j < n; j++ {
+        ss.WriteByte(s[i+j])
+    }
+    return ss.String()
 }
