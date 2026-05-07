08b09ffdc3a8247511d67e73e5bec764c9462e5f
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2026-05-07 11:45:17 +0530

    Time: 39 ms (28.57%), Space: 33.7 MB (14.29%) - LeetHub

---
+++ 3660-jump-game-ix/3660-jump-game-ix.go
@@ -0,0 +1,30 @@
+func maxValue(nums []int) []int {
+	n := len(nums)
+	ans := make([]int, n)
+	type Item struct {
+		value int
+		left  int
+		right int
+	}
+
+	stack := make([]Item, 0)
+	for i := 0; i < n; i++ {
+		curr := Item{nums[i], i, i}
+
+		for len(stack) > 0 && stack[len(stack)-1].value > nums[i] {
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if top.value > curr.value {
+				curr.value = top.value
+			}
+			curr.left = top.left
+		}
+		stack = append(stack, curr)
+	}
+	for i := 0; i < len(stack); i++ {
+		for j := stack[i].left; j <= stack[i].right; j++ {
+			ans[j] = stack[i].value
+		}
+	}
+	return ans
+}
