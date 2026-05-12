8286bd898f27ea07a66fd41c8b436f14219c885b
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2026-05-12 10:58:38 +0530

    Time: 21 ms (6.25%), Space: 20 MB (25%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.go
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.go
@@ -1,16 +1,14 @@
 func minimumEffort(tasks [][]int) int {
-    for i := range tasks {
-        tasks[i] = append(tasks[i], tasks[i][1] - tasks[i][0])
-    }
     sort.Slice(tasks, func (i, j int) bool {
-        return tasks[i][2] > tasks[j][2]
+        return tasks[i][1] - tasks[i][0] > tasks[j][1] - tasks[j][0]
     })
 
-    var l, bal, loan int = len(tasks), tasks[0][2], 0
+    var l, bal, loan int = len(tasks), tasks[0][1]-tasks[0][0], 0
     for i:=1; i < l; i++ {
-        if bal < tasks[i][1] {
-            loan += tasks[i][1] - bal
-            bal = tasks[i][1]
+        var spend int = tasks[i][1]
+        if bal < spend {
+            loan += spend - bal
+            bal = spend
         }
         bal -= tasks[i][0]
     }
