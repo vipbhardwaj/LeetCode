5d49cadef4e9fb6c35350d9a961ba5c277d5a5e3
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2026-06-01 09:18:37 +0530

    Time: 0 ms (100%), Space: 4.3 MB (60%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.go
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.go
@@ -2,7 +2,6 @@ func minimumCost(cost []int) int {
     sort.Slice(cost, func(i, j int) bool {
         return cost[i] > cost[j]
     })
-    // fmt.Println(cost)
     var res int
     for i, c := range cost {
         if (i+1) % 3 == 0 {
