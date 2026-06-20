cdaeda4ee2f17c4021120af714a9c1a67ed1cc68
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2026-06-20 19:32:52 +0530

    Time: 99 ms (53.85%), Space: 20.9 MB (26.92%) - LeetHub

---
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.go
@@ -0,0 +1,46 @@
+func countSmaller(nums []int) []int {
+    n := len(nums)
+    pairs := make([][]int, n)
+    temp := make([][]int, n)
+    ans := make([]int, n)
+    for i, v := range nums {
+        pairs[i] = []int{v, i}
+        temp[i] = []int{v, i}
+    }
+    
+    merge_sort(0, n-1, ans, pairs, temp)
+    return ans
+}
+
+func merge_sort(s, e int, ans []int, pairs, temp [][]int) {
+    if s >= e { return }
+    m := s + (e - s) / 2
+    merge_sort(s, m, ans, pairs, temp)
+    merge_sort(m+1, e, ans, pairs, temp)
+
+    l, r, i, nRightLessThanLeft := s, m + 1, s, 0
+    for l <= m && r <= e {
+        if pairs[l][0] <= pairs[r][0] {
+            ans[pairs[l][1]] += nRightLessThanLeft
+            temp[i] = pairs[l]
+            l++
+        } else {
+            temp[i] = pairs[r]
+            nRightLessThanLeft++
+            r++
+        }
+        i++
+    }
+    for l <= m {
+        ans[pairs[l][1]] += nRightLessThanLeft
+        temp[i] = pairs[l]
+        i++
+        l++
+    }
+    for r <= e {
+        temp[i] = pairs[r]
+        i++
+        r++
+    }
+    for j:=s; j<=e; j++ { pairs[j] = temp[j] }
+}
