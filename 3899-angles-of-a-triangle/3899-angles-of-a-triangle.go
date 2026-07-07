9a625a7fa37168321881664d33cef40dfa5f64dc
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2026-07-07 21:31:19 +0530

    Time: 0 ms (100%), Space: 4.3 MB (77.78%) - LeetHub

---
+++ 3899-angles-of-a-triangle/3899-angles-of-a-triangle.go
@@ -0,0 +1,19 @@
+func internalAngles(sides []int) []float64 {
+    // m, mi := 0, 0
+    // for i, s := range sides {
+    //     if s > m {
+    //         m = s
+    //         mi = i
+    //     }
+    // }
+    // if sides[mi]
+
+    slices.Sort(sides)
+    a, b, c := sides[0], sides[1], sides[2]
+    if a + b <= c { return []float64{} }
+
+    angleC := math.Acos(float64(a*a + b*b - c*c) / float64(2*a*b)) * 180 / math.Pi
+    angleB := math.Acos(float64(a*a + c*c - b*b) / float64(2*a*c)) * 180 / math.Pi
+    angleA := 180 - angleB - angleC
+    return []float64{angleA, angleB, angleC}
+}
