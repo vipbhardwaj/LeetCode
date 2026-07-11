b1f0b78c89ccceb8667c5f7777e0947a16618f51
VIPUL BHARDWAJ <75437272+vipbhardwaj@users.noreply.github.com>
2026-07-11 12:49:38 +0530

    Time: 23 ms (40%), Space: 9.5 MB (30%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.go
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.go
@@ -8,19 +8,19 @@ func countCompleteComponents(n int, edges [][]int) int {
     var res int
     for i := range n {
         if v[i] {continue}
-        var compInfo = []int{0, 0}
-        dfs(i, adj, v, compInfo)
-        if compInfo[0] * (compInfo[0]-1) == compInfo[1] {res++}
+        var nodes_edges = []int{0, 0}
+        dfs(i, adj, v, nodes_edges)
+        if nodes_edges[0] * (nodes_edges[0]-1) == nodes_edges[1] {res++}
     }
     return res
 }
 
-func dfs(n int, adj [][]int, v map[int]bool, c []int) {
-    v[n] = true
-    c[0]++
-    c[1] += len(adj[n])
-    for _, j := range adj[n] {
+func dfs(node int, adj [][]int, v map[int]bool, nodes_edges []int) {
+    v[node] = true
+    nodes_edges[0]++
+    nodes_edges[1] += len(adj[node])
+    for _, j := range adj[node] {
         if v[j] {continue}
-        dfs(j, adj, v, c)
+        dfs(j, adj, v, nodes_edges)
     }
 }
